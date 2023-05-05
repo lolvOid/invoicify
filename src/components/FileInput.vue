e073a10a965ee8c7501932f4a57fe4356d118a36
void <linhtet1998itachi@gmail.com>
2023-05-05 19:37:11 +0000

    Update 2 files

---
+++ src/components/FileInput.vue
@@ -0,0 +1,12 @@
+<template>
+    <input type="date" class="rounded-md p-1 border-2 bg-slate-200" placeholder="12334435345" />
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    name: 'FileInput',
+    data() {},
+    methods: {},
+}
+</script>
