d4405cd3b39e8f43f64aa4b703393ee8cc53d26a
lolvOid <linhtet1998itachi@gmail.com>
2023-05-07 14:19:01 +0100

    Updated GA

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
-
+import { VitePluginRadar } from 'vite-plugin-radar'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         svgLoader({
             svgo: false,
+        }),
+        VitePluginRadar({
+            // Google Analytics tag injection
+            analytics: [
+                {
+                    id: 'G-B6XTCBGQRB',
+                    consentDefaults: {
+                        analytics_storage: 'granted',
+                        ad_storage: 'denied',
+                        wait_for_update: 500,
+                    },
+                    persistentValues: {
+                        currency: 'USD',
+                    },
+                },
+            ],
         }),
     ],
 })
