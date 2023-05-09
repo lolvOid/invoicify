763a2bc65d9ce5ffdce3fa17780cbe38ccf754ce
lolvOid <linhtet1998itachi@gmail.com>
2023-05-09 16:47:18 +0100

    Updated Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -9,7 +9,32 @@ export default defineConfig({
         svgLoader({
             svgo: false,
         }),
-       
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets:['logo-192.png'],
+            manifest: {
+                name: 'INVOICIFY',
+                short_name: 'INVOICIFY',
+                description: 'Simple invoice maker',
+                theme_color: '#ffffff',
+                icons: [
+                  {
+                    src: 'logo-192.png',
+                    sizes: '192x192',
+                    type: 'image/png'
+                  },
+                  {
+                    src: 'invoicify.png',
+                    sizes: '512x512',
+                    type: 'image/png'
+                  }
+                ]
+              },
+            injectRegister:'auto',
+            devOptions: {
+              enabled: true
+            }
+          }) 
     ],
     server: {
         host: true
