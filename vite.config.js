91537a18145ecab5d5faf8e61ec163e8c7fa4d93
lolvOid <linhtet1998itachi@gmail.com>
2023-05-09 17:02:53 +0100

    Updated

--- vite.config.js
+++ vite.config.js
@@ -65,7 +65,7 @@ export default defineConfig({
               },
             injectRegister:'auto',
             devOptions: {
-              enabled: true
+              enabled: false
             },
             workbox: {
                 cleanupOutdatedCaches: false
